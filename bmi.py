0788bb3a0f5f3ab113a69f5a36e35d1b6111b08f
Your Name <you@example.com>
2024-11-21 11:48:30 +0100

    Sanya.assign

--- bmi.py
+++ bmi.py
@@ -1,11 +1,13 @@
 weight = float(input('Enter your weight in kgs: '))
 height = float(input('Enter your height in meters: '))
 
-if weight <= 0 or height <= 0:
-    print('Error: Weight and Height must be positive numbers.')
-
-
 bmi = weight / (height **2)
+if weight == 0 or height ==0:
+    print('Oops: Enter a positive value and try again...')
+elif weight <= 0 or height <= 0:
+    print('Error!!!: Enter a positive value')    
+
+
 if bmi < 18.5:
     print(f'Your BMI is{bmi:.2f}. You are classified as: Underweight.')  
 elif 18.5 <= bmi <25:
--- ttc.py
+++ ttc.py
@@ -2,22 +2,24 @@ a = float(input("Enter the length of the first side: "))
 b = float(input("Enter the length of the second side: "))
 c = float(input("Enter the length of the third side: "))
         
-        # Validate triangle inequality
 if a <= 0 or b <= 0 or c <= 0:
     print("Error: Side lengths must be positive numbers.")
-        
+    
+        #using inequlity theorem
 if a + b <= c or a + c <= b or b + c <= a:
     print("Error: The given side lengths do not form a valid triangle.")
        
         # Determine triangle type
 if a == b == c:
-    triangle_type = "Equilateral"
-elif a == b or b == c or a == c:
-    triangle_type = "Isosceles"
-else:
-    triangle_type = "Scalene"
-        
-    print(f"The triangle is {triangle_type}.")    
+    print(f"The triangle is Equilateral.") 
 
-    print("Error: Please enter valid numbers.")
+elif a == b or b == c or a == c:
+    print(f"The triangle is Isosceles.")  
+    
+else:    
+    print(f"The triangle is Scalene.")    
+
+if a + b + c >= 181:
+    print('Invalid triangle sides')
+
 
